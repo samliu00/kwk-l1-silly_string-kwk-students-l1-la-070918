ff47b37a2d46795162cfc81ea9c98aadeaaaa896
samliu00 <samanthaliu19@gmail.com>
2018-07-09 22:12:30 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,9 +1,9 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = "discombobulate"
+your_favorite_word = ""
 
-puts "My favorite word is #{discombobulate}"
+puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
